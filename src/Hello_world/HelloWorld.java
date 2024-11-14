459ded8b9bc82e67f65863f293f2dbdd3fd55e8e
Salim-hubs <mohamed.ceddah@etu.univ-amu.fr>
2024-11-14 15:34:01 +0100

    test 2 salim

--- src/Hello_world/HelloWorld.java
+++ src/Hello_world/HelloWorld.java
@@ -3,5 +3,10 @@ package Hello_world;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+
+
+        System.out.println("ta vieille daronne qui suce roland");
     }
 }
